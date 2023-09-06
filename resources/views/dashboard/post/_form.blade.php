d0881e75956ec4bc94daaeae47d7b2be4bd5d2fa
cristobal pablo <torrijos.arguello.1cm@gmail.com>
2023-09-05 22:02:49 -0600

    Proyecto crud

---
+++ resources/views/dashboard/post/_form.blade.php
@@ -0,0 +1,42 @@
+@csrf
+
+        <label for="">Titulo</label>
+        <input type="text" name="title" value="{{ old('title', $post->title) }}">
+
+        <label for="">Slug</label>
+        <input type="text" name="slug" value="{{ old('slug', $post->slug) }}">
+
+
+        <label for="">Categoria</label>
+        <select name="category_id" value="{{ old('title', '') }}">
+            <option value=""></option>
+            @foreach ($categories as $title => $id)
+                <option {{ old('category_id', "$post->category_id") == $id ? 'selected' : '' }} value="{{ $id }}">
+                    {{ $title }}
+                </option>
+            @endforeach
+        </select>
+
+        <label for="">Posteado</label>
+
+        <select name="posted">
+            <option {{ old('posted',$post->posted )== 'yes' ? 'selected' : '' }} value="yes">si</option>
+            <option {{ old('posted',$post->posted) == 'not' ? 'selected' : '' }} value="not">no</option>
+        </select>
+
+        <label for="">Contenido</label>
+        <textarea name="content">{{ old('content',$post->content ) }}</textarea>
+
+
+        <label for="">Descripcion</label>
+        <textarea name="description">{{ old('description',$post->description) }}</textarea>
+
+        @if (isset ($task) && $task=="edit")
+        <label for="">imagen</label>
+        <input type="file" name="image">
+            
+        @endif
+
+
+
+        <button type="submit">Enviar</button>
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Dashboard\PostController;
+use Illuminate\Foundation\Console\RouteListCommand;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('post', PostController::class);
+
+// Route::get('post', [PostController::class,'index']);
+// Route::get('post/{post}', [PostController::class,'show']);
+// Route::get('post/create', [PostController::class,'create']);
+// Route::get('post/{post}/edit', [PostController::class,'edit']);
+
+// Route::post('post', [PostController::class,'store']);
+// Route::pust('post/{post}', [PostController::class,'update']);
+// Route::pust('post/{post}', [PostController::class,'delete']);
